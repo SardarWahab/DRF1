da8ad08ad9d3cc23b3a4b290ada26b3e6dee6522
SardarWahab <wahabsardar130@gmail.com>
2024-11-12 21:52:58 +0500

    "Updated views.py to import TaskSerializer and use it to serialize tasks, and modified the get_notes function accordingly."

--- drf1/task/views.py
+++ drf1/task/views.py
@@ -3,10 +3,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Task
+from .serializers import TaskSerializer
 # Create your views here.
 
 @api_view(['GET'])
 def get_notes(request):
-#     notes = Task.objects.all()
-#     serializer = NoteSerializer(notes, many=True)
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(notes, many=True)
     return Response("Hallo World")
