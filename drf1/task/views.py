528d320b0df94bb05a2688f530873bd6070bb6d2
SardarWahab <wahabsardar130@gmail.com>
2024-11-12 21:34:14 +0500

    "Updated drf1 project: modified urls.py, added new view in views.py, and updated pycache files"

--- drf1/task/views.py
+++ drf1/task/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
 # Create your views here.
+
+@api_view(['GET'])
+def get_notes(request):
+#     notes = Task.objects.all()
+#     serializer = NoteSerializer(notes, many=True)
+    return Response("Hallo World")
