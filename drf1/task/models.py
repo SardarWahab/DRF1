6625bbd0bb82945bf3481ba51cdc4a19ca323296
SardarWahab <wahabsardar130@gmail.com>
2024-11-12 21:23:34 +0500

    First

---
+++ drf1/task/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, blank=True,null=True)
+    description = models.TextField( blank=True,null=True)
