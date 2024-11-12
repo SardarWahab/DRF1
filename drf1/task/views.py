ed6ee643cd73a7569d6872d0634448ac429fd938
SardarWahab <wahabsardar130@gmail.com>
2024-11-12 21:54:23 +0500

    "Updated views.py to import TasksSerializers instead of TaskSerializer and use it for serialization."

--- drf1/task/views.py
+++ drf1/task/views.py
@@ -3,11 +3,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Task
-from .serializers import TaskSerializer
+from .serializers import TasksSerializers
 # Create your views here.
 
 @api_view(['GET'])
 def get_notes(request):
     tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
+    serializer = TasksSerializers(tasks, many=True)
     return Response(serializer.data)
